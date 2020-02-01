61b85d189d980c621752a7ab04283cc7fb5a4f76
Сапегин Сергей Владимирович <s.sapegin@2035.university>
2020-02-01 14:10:33 +0300

    killing bug with noparam

--- resources/info.py
+++ resources/info.py
@@ -2,11 +2,12 @@ from .wrapper import Resource
 from flask import make_response, jsonify
 
 class Info(Resource):
-    def get(self, param_name):
-        mess = "Hello, with %s param!" % param_name
+    def get(self, param_name = ''):
+        if param_name == '':
+            mess = "Im working correct, nothing bad"
+        else:
+            mess = "Hello, with %s param!" % param_name
         return make_response(jsonify(message=mess), 200)
 
-    def get(self):
-        mess = "Im working correct, nothing bad"
-        return make_response(jsonify(message=mess), 200)
+
 
